fbb335217a3bd2347ac1e3f4013fb2917135c908
Jliezed <103245265+Jliezed@users.noreply.github.com>
2022-08-08 10:30:58 +0200

    Final Optimized Glouton

--- optimized_glouton.py
+++ optimized_glouton.py
@@ -1,9 +1,10 @@
 import pandas
 import timeit
+from guppy import hpy
 
 # -------------------- READ FILE --------------------
-original_dataset = pandas.read_csv("./data/data.csv", delimiter=";")
-#print(original_dataset)
+filename = "data.csv"
+original_dataset = pandas.read_csv("./data/" + filename, delimiter=",")
 
 # -------------------- GET BASIC INFO --------------------
 original_dataset.describe()
@@ -16,12 +17,10 @@ dataset_new_headers = original_dataset.rename(columns={
     "profit": "PROFIT_PERCENTAGE"
 })
 
-#print(dataset_new_headers)
 
 # -------------------- ADD NEW CALCULATED COLUMN --------------------
 dataset_new_headers["PROFIT_VOLUME"] = dataset_new_headers["COST"] * \
                                        (dataset_new_headers["PROFIT_PERCENTAGE"]/100)
-#print(dataset_new_headers)
 
 dataset_new_headers["PROFIT_FOR_1"] = ((dataset_new_headers["PROFIT_PERCENTAGE"]/100) \
                                       * dataset_new_headers["COST"]) / \
@@ -34,44 +33,37 @@ print(dataset)
 data_sort_by_profit_percentage = dataset.sort_values(by=["PROFIT_PERCENTAGE"],
                                                 ascending=False,
                                                 na_position="first")
-#print(data_sort_by_profit_percentage)
 
 data_sort_by_profit_volume = dataset.sort_values(by=["PROFIT_VOLUME"],
                                             ascending=False,
                                             na_position="first")
-#print(data_sort_by_profit_volume)
+
 data_sort_by_cost_asc = dataset.sort_values(by=["COST"],
                                        ascending=True,
                                        na_position="first")
-#print(data_sort_by_cost_asc)
+
 data_sort_by_cost_desc = dataset.sort_values(by=["COST"],
                                        ascending=False,
                                        na_position="first")
-#print(data_sort_by_desc)
+
 data_sort_by_profit_for_1 = dataset.sort_values(by=["PROFIT_FOR_1"],
                                        ascending=False,
                                        na_position="first")
-#print(data_sort_by_profit_for_1)
-
-# ITERATE OVER ROWS
 
 
-def calculateProfit(dataset, MAX_SPEND = 500):
+def calculateProfit(dataset, max_spend=500):
     total_action_cost = 0
     total_profit = 0
 
     for index, (action, profit) in enumerate(zip(dataset["COST"],
                                dataset["PROFIT_VOLUME"])):
-        # Define previous values
-        # prev_action_cost = dataset['COST'].iloc[index - 1]
-        # prev_profit = dataset["PROFIT_VOLUME"].iloc[index - 1]
 
         # Add cost & profit to total
         total_action_cost += action
         total_profit += profit
 
         # Check if total cost over max spend
-        if total_action_cost > MAX_SPEND:
+        if total_action_cost > max_spend:
             total_action_cost -= action
             total_profit -= profit
 
@@ -80,12 +72,42 @@ def calculateProfit(dataset, MAX_SPEND = 500):
                  f" {total_profit}")
 
 
-starttime = timeit.default_timer()
-print("The start time is :",starttime)
+start_time = timeit.default_timer()
 calculateProfit(dataset)
 calculateProfit(data_sort_by_profit_percentage)
 calculateProfit(data_sort_by_profit_volume)
 calculateProfit(data_sort_by_cost_asc)
 calculateProfit(data_sort_by_cost_desc)
 calculateProfit(data_sort_by_profit_for_1)
-print("The time difference is :", timeit.default_timer() - starttime)
+print("The execution time is :", timeit.default_timer() - start_time, "sec")
+
+
+# Calculate space used
+heap = hpy()
+
+print("Heap Status At Starting : ")
+heap_status1 = heap.heap()
+print("Heap Size : ", heap_status1.size, " bytes\n")
+print(heap_status1)
+
+heap.setref()
+
+print("\nHeap Status After Setting Reference Point : ")
+heap_status2 = heap.heap()
+print("Heap Size : ", heap_status2.size, " bytes\n")
+print(heap_status2)
+
+calculateProfit(dataset)
+calculateProfit(data_sort_by_profit_percentage)
+calculateProfit(data_sort_by_profit_volume)
+calculateProfit(data_sort_by_cost_asc)
+calculateProfit(data_sort_by_cost_desc)
+calculateProfit(data_sort_by_profit_for_1)
+
+print("\nHeap Status After Creating Few Objects : ")
+heap_status3 = heap.heap()
+print("Heap Size : ", heap_status3.size, " bytes\n")
+print(heap_status3)
+
+print("\nMemory Usage After Creation Of Objects : ",
+      heap_status3.size - heap_status2.size, " bytes")
